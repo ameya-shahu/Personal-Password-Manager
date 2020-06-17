bc00cc844ed0583a0b188afe4b1af9ee9868c528
ameya-shahu <ameyashahu@gmail.com>
2020-06-17 20:25:21 +0530

    copy to clipboard functionality added

--- app/src/main/java/com/example/passwordmanager/CredentialAdapter.kt
+++ app/src/main/java/com/example/passwordmanager/CredentialAdapter.kt
@@ -1,39 +1,66 @@
 package com.example.passwordmanager
 
-import android.app.AlertDialog
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
+import android.renderscript.RenderScript
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.list_item_layout.view.*
+import android.content.Context.CLIPBOARD_SERVICE as CLIPBOARD_SERVICE1
 
-class CredentialAdapter(
-    private val items: List<CredentialsModel>,
-    ctx: Context, val clickListener: (CredentialsModel) -> Unit
-): RecyclerView.Adapter<CredentialAdapter.ViewHolder>() {
+//private var items: MutableList<CredentialsModel>,
+
+class CredentialAdapter(ctx: Context, val credentialList : ArrayList<CredentialsModel>): RecyclerView.Adapter<CredentialAdapter.ViewHolder>() {
     var context = ctx
-    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
-        fun bind(credential: CredentialsModel,clickListener: (CredentialsModel) -> Unit){
-            itemView.urlView.text = credential.url
-            itemView.userNameView.text = credential.userName
-            itemView.passwordView.text = credential.password
-            itemView.noteView.text = credential.note
-            itemView.delButton.setOnClickListener{clickListener(credential)}
-        }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val urlView = itemView.findViewById(R.id.urlView) as TextView
+        val userNameView = itemView.findViewById(R.id.userNameView) as TextView
+        val passwordView = itemView.findViewById(R.id.passwordView) as TextView
+        val noteView = itemView.findViewById(R.id.noteView) as TextView
+        val delButton = itemView.findViewById(R.id.delButton) as Button
+        val cpyButton = itemView.findViewById(R.id.cpyButton) as Button
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item_layout,parent,false))
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item_layout, parent, false)
+        )
     }
 
     override fun getItemCount(): Int {
-        return items.size
+        return credentialList.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val credential:CredentialsModel = items[position]
-        holder.bind(credential,clickListener)
-    }
+        val credential: CredentialsModel = credentialList[position]
+        holder.urlView.text = credential.url
+        holder.userNameView.text = credential.userName
+        holder.passwordView.text = credential.password
+        holder.noteView.text = credential.note
 
+        holder.delButton.setOnClickListener {
+            val db = DataBaseHandler(context)
+            if (db.deleteData(credential.id)) {
+                credentialList.removeAt(holder.getAdapterPosition())
+                notifyItemRemoved(position)
+            }
+        }
+
+        holder.cpyButton.setOnClickListener {
+            val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clipUser = ClipData.newPlainText("username", credential.userName)
+            clipboard.setPrimaryClip(clipUser)
+            Toast.makeText(context, "Copied username to clipboard", Toast.LENGTH_SHORT).show()
+            val clipPass = ClipData.newPlainText("password", credential.password)
+            clipboard.setPrimaryClip(clipPass)
+            Toast.makeText(context, "Copied password to clipboard", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
--- app/src/main/java/com/example/passwordmanager/MainActivity.kt
+++ app/src/main/java/com/example/passwordmanager/MainActivity.kt
@@ -4,29 +4,39 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL,false)
-        val db = DataBaseHandler(this)
-        val detailsData = db.readCredentials()
-        val adapter = CredentialAdapter(detailsData,this,{credentialsModel: CredentialsModel->deleteClick(credentialsModel)})
-        recyclerView.adapter = adapter
-    }
 
-    fun deleteClick(credential: CredentialsModel){
         val db = DataBaseHandler(this)
-        if(db.deleteData(credential.id)){
-            //adapter.notifyItemRemoved(position)
-            Toast.makeText(applicationContext,"Deleted", Toast.LENGTH_SHORT).show()
+        val detailsData = ArrayList(db.readCredentials())
+        val adapter = CredentialAdapter(this,detailsData)
+        recyclerView.adapter = adapter
+        if(detailsData.isEmpty()){
+            recyclerView.visibility = View.INVISIBLE
+            findViewById<TextView>(R.id.emptyView).visibility = View.VISIBLE
+        }
+        else{
+            recyclerView.visibility = View.VISIBLE
+            findViewById<TextView>(R.id.emptyView).visibility = View.INVISIBLE
         }
     }
+
+//    fun deleteClick(credential: CredentialsModel){
+//        val db = DataBaseHandler(this)
+//        if(db.deleteData(credential.id)){
+//            //adapter.notifyItemRemoved(position)
+//            Toast.makeText(applicationContext,"Deleted", Toast.LENGTH_SHORT).show()
+//        }
+//    }
 
     fun addNewCredentials(view : View){
         print("hello world")
